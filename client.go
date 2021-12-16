9514b11828c342cfc5292b13df0068806683e354
Miguel Fonseca <miguel@xiatech.co.uk>
2021-12-16 17:10:57 +0000

    Disable un-tested functionality from the client

--- client.go
+++ client.go
@@ -9,10 +9,6 @@ import (
 )
 
 const (
-	// TODO:
-	//  I've had to bump this to 2021-04 (from 2021-01 which is what upstream supports) to support price lists
-	//  I wonder if we should disabled all of the remaining functionality (besides price lists) that we don't know if it works with the new API version?
-	//  There's also pretty much no coverage for all of the upstream functionality...
 	shopifyAPIVersion = "2021-04"
 )
 
@@ -54,19 +50,25 @@ func NewDefaultClient() (shopClient *Client) {
 	return
 }
 
+// NewClient returns a new client based on the configuration arguments received.
+// Most of this client functionality is not enabled since it has no test coverage.
+// Although it should, we cannot be sure that it works on the current api version since we haven't tested it.
+// Enabling should require adding test coverage and proper testing.
+// Calling any of the disabled (commented below) functionaly will likely result in a nil pointer de-reference.
 func NewClient(apiKey string, password string, storeName string) *Client {
 	c := &Client{gql: newShopifyGraphQLClient(apiKey, password, storeName)}
 
 	c.BulkOperation = &BulkOperationServiceOp{client: c}
 
-	c.Product = &ProductServiceOp{client: c}
-	c.Variant = &VariantServiceOp{client: c}
-	c.Inventory = &InventoryServiceOp{client: c}
-	c.Collection = &CollectionServiceOp{client: c}
-	c.Order = &OrderServiceOp{client: c}
-	c.Fulfillment = &FulfillmentServiceOp{client: c}
-	c.Location = &LocationServiceOp{client: c}
-	c.Metafield = &MetafieldServiceOp{client: c}
+	// c.Product = &ProductServiceOp{client: c}
+	// c.Variant = &VariantServiceOp{client: c}
+	// c.Inventory = &InventoryServiceOp{client: c}
+	// c.Collection = &CollectionServiceOp{client: c}
+	// c.Order = &OrderServiceOp{client: c}
+	// c.Fulfillment = &FulfillmentServiceOp{client: c}
+	// c.Location = &LocationServiceOp{client: c}
+	// c.Metafield = &MetafieldServiceOp{client: c}
+
 	c.PriceList = &PriceListServiceOp{c.BulkOperation, c.gql}
 
 	return c
